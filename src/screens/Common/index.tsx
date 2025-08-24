22277d4633619fa6f9fc99c53f72cdfa53752a9e
madushpro <madushat@heptagon.lk>
2025-08-24 19:16:47 +0530

    ad

--- src/screens/Common/index.tsx
+++ src/screens/Common/index.tsx
@@ -1,46 +1,46 @@
-import { StyleSheet, Text, View } from "react-native";
-import React, { useEffect } from "react";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
-import Header from "../../components/Common/Header";
-import SearchBar from "../../components/Common/Searchbar";
-import { NewsDataType } from "../../components/Types/index";
-import axios from "axios";
+// import { StyleSheet, Text, View } from "react-native";
+// import React, { useEffect } from "react";
+// import { useSafeAreaInsets } from "react-native-safe-area-context";
+// import Header from "../../components/Common/Header";
+// import SearchBar from "../../components/Common/Searchbar";
+// import { NewsDataType } from "../../components/Types/index";
+// import axios from "axios";
 
-type Props = {}
+// type Props = {}
 
-const Page: React.FC<Props> = (props) => {
-    const {top:safeTop} = useSafeAreaInsets();
-    const [breakingNews, setBreakingNews] = React.useState<NewsDataType[]>([]);
+// const Page: React.FC<Props> = (props) => {
+//     const {top:safeTop} = useSafeAreaInsets();
+//     const [breakingNews, setBreakingNews] = React.useState<NewsDataType[]>([]);
 
-    useEffect(() => {
+//     useEffect(() => {
 
-    const getBreakingNews = async () => {
-        try {
-            const URL = `https://newsdata.io/api/1/latest?apikey=${process.env.TESTAPIKEY}&country=us&prioritydomain=top`
-            const response = await axios .get(URL);
+//     const getBreakingNews = async () => {
+//         try {
+//             const URL = `https://newsdata.io/api/1/latest?apikey=${process.env.TESTAPIKEY}&country=us&prioritydomain=top`
+//             const response = await axios .get(URL);
 
-            if(response && response.data) {
-                console.log("Breaking News:", response.data);
-            }
-    }
-}
+//             if(response && response.data) {
+//                 console.log("Breaking News:", response.data);
+//             }
+//     }
+// }
 
-  return (
-    <View style={[styles.container, {paddingTop: safeTop}]}>
-      <Header/> 
-      <SearchBar /> 
-    </View>     
-  );
-}   
-export default Page;
+//   return (
+//     <View style={[styles.container, {paddingTop: safeTop}]}>
+//       <Header/> 
+//       <SearchBar /> 
+//     </View>     
+//   );
+// }   
+// export default Page;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    // Add other default styles as needed
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     // Add other default styles as needed
+//   },
+// });
 
 
 
